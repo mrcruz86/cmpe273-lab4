6aba6678e69cec55ef058bcee8a8a4afb07d5997
mrcruz86 <jason@Jason-Cruzs-MacBook-Pro.local>
2013-11-09 11:14:29 -0800

    Upload Lab to Github

--- library/src/main/resources/assets/js/app.js
+++ library/src/main/resources/assets/js/app.js
@@ -1,4 +1,17 @@
 $(":button").click(function() {
-	var isbn = this.id;
-	alert('About to report lost on ISBN ' + isbn);
+	var status = $("#status").text();
+	if (!(status == "lost")){
+		var isbn = this.id;
+		alert('About to report lost on ISBN ' + isbn);
+	
+		$.ajax({
+			url : "http://localhost:8001/library/v1/books/" + isbn + "?status=lost",
+			statusCode: {
+				200: function() {
+					$("#"+isbn+"s").text("lost");
+					$("#"+isbn).prop("disabled", true);
+				}
+			}
+		});
+	}
 });
